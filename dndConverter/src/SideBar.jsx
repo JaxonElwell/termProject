881bd59c9cc94fff1b1819798723efd4f0945975
JElwellMSU <jelwell@murraystate.edu>
2024-11-01 02:45:27 -0400

    Baseline Theme and Layout
    Added a sidebar layout for main tabs for application
    
    Installed react-icons and tailwindcss

---
+++ dndConverter/src/SideBar.jsx
@@ -0,0 +1,34 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { BsFillCollectionFill } from "react-icons/bs";
+import { FaHome } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
+
+const SideBar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-white dark:bg-gray-900 shadow-lg">
+                    
+        <SideBarIcon icon={<FaHome size="28" />} />
+        <Divider />
+        <SideBarIcon icon={<BsPlus size="38" />} />
+        <SideBarIcon icon={<BsFillCollectionFill  size="28" />} />
+        <Divider />
+        <SideBarIcon icon={<IoPersonSharp size="28" />} />
+        <SideBarIcon icon={<BsGearFill size="28" />} />
+    </div>
+  );
+};
+
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span class="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
+);
+
+
+const Divider = () => <hr className="sidebar-hr" />;
+
+export default SideBar;
